daf83b7f6838837c752fd68edd28e0cc932c25ca
Kacper61 <37276876+Kacper61@users.noreply.github.com>
2018-05-30 12:18:03 +0200

    Add files via upload

---
+++ loger final/loger final/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace loger_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Wartość = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.DataSource = this.chart1.Titles;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(417, 41);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(38, 218);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 34);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Podaj kierunek przekroczenia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Podaj adres logu";
+            // 
+            // Wartość
+            // 
+            this.Wartość.AutoSize = true;
+            this.Wartość.Location = new System.Drawing.Point(43, 264);
+            this.Wartość.Name = "Wartość";
+            this.Wartość.Size = new System.Drawing.Size(91, 13);
+            this.Wartość.TabIndex = 6;
+            this.Wartość.Text = "Wartość progowa";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 284);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Wartość);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "logger temperatury";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Wartość;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ loger final/loger final/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+
+using System.IO;
+
+using System.IO.Ports;
+
+namespace loger_final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class komunikacja
+    {
+        int t;
+        int k;
+        string naz;
+        public void sprawdzporty()
+        {
+            {
+                string[] porty = SerialPort.GetPortNames();
+
+                int i = 0;
+                foreach (string port in porty)
+                {
+                    Console.WriteLine(port);
+                    i++;
+                }
+
+            }
+        }
+        public void rs(string a)
+        {
+            SerialPort port = new SerialPort(a,
+                    9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.WriteLine("1");
+            Console.WriteLine(port.ReadLine());
+            port.WriteLine("3");
+            port.WriteLine("5");
+            port.WriteLine("7");
+            port.Close();
+            naz = a;
+        }
+        public void nastawa(int b)
+        {
+            t = b;
+
+        }
+        public void kierunek(int b)
+        {
+            k = b;
+
+        }
+        public string Sprawdz(string str)
+        {
+
+            int temp;
+            SerialPort port1 = new SerialPort(naz,
+                   9600, Parity.None, 8, StopBits.One);
+            port1.Open();
+            port1.WriteLine("1");
+
+            temp = Int32.Parse(port1.ReadLine());
+            port1.Close();
+            if (k == 0)
+            {
+                if (temp < (t * 10))
+                {
+                    port1.Open();
+                    port1.WriteLine("4");
+                    port1.WriteLine("6");
+                    port1.Close();
+                    return ((Convert.ToString(temp / 10)) + " " + str);
+                }
+
+            }
+            if (k == 1)
+            {
+                if (temp > (t * 10))
+                {
+                    port1.Open();
+                    port1.WriteLine("2");
+                    port1.WriteLine("6");
+                    port1.Close();
+                    return ((Convert.ToString(temp / 10)) + " " + str);
+                }
+
+            }
+            port1.Open();
+            port1.WriteLine("7");
+            port1.WriteLine("3");
+            port1.WriteLine("5");
+            port1.Close();
+            return "a";
+        }
+    }
+
+    class zapis
+    {
+        string naz;
+        public void utworz(string n)
+        {
+            naz = n;
+            if (!File.Exists(n))
+            {
+                //File.CreateText(n);
+                Console.WriteLine("log utworzony");
+
+
+            }
+        }
+
+        public void dopisz(string s)
+
+        {
+
+
+
+
+
+            StreamWriter sw = new StreamWriter(naz, append: true);
+            // StreamWriter sw = File.CreateText(naz);
+            if (s != "a")
+            {
+                sw.WriteLine(s, "/n");
+            }
+
+            sw.Close();
+
+
+
+
+        }
+    }
+
+
+    /*  public class czasomierz
+       {
+
+
+           public void start()
+
+           {
+
+
+               Timer czas = new System.Timers.Timer();
+               czas.Interval = 2000;
+               czas.Elapsed += TimedEvent;
+               czas.AutoReset = true;
+
+               czas.Enabled = true;
+           }
+              public  void TimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+               {
+                   Console.WriteLine("czas", e.SignalTime);
+
+
+               }
+
+
+
+       }
+       */
+
+
+
+
+
+
+
+    public static void Main(string[] args)
+    {
+
+
+
+        komunikacja k1 = new komunikacja();
+        zapis z1 = new zapis();
+        Console.WriteLine("dostępne porty");
+        k1.sprawdzporty();
+        Console.WriteLine("Wpisz port, pod którym jest logger");
+        k1.rs(Console.ReadLine());
+        Console.WriteLine("Podaj temperaturę progową");
+        k1.nastawa(Int32.Parse(Console.ReadLine()));
+        Console.WriteLine("Podaj kierunek przekroczenia ( 1- w górę, 0 - w dół");
+        k1.kierunek(Int32.Parse(Console.ReadLine()));
+        Console.WriteLine("Podaj ścieżkę do logu w formacie c:\\log\\nazwalogu.txt");
+
+        z1.utworz(Console.ReadLine());
+        System.Timers.Timer aTimer;
+        aTimer = new System.Timers.Timer();
+        aTimer.Interval = 2000;
+
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+
+        // Have the timer fire repeated events (true is the default)
+        aTimer.AutoReset = true;
+
+        // Start the timer
+        aTimer.Enabled = true;
+
+
+        Console.ReadLine();
+
+        void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("czas {0}", e.SignalTime);
+            z1.dopisz(k1.Sprawdz(Convert.ToString(e.SignalTime)));
+        }
+
+        /*    void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("czas", e.SignalTime);
+                z1.dopisz(k1.Sprawdz());
+
+        }
+        */
+        Console.ReadKey();
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
